4c0e75c47cab2ec55eb072568e7420c8c7703df4
DivyaPimple <95083581+DivyaPimple@users.noreply.github.com>
2021-11-26 17:59:38 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,103 +1,97 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include<conio.h>
+/***********************************
+ Implementation of BINARY SEARCH
+***********************************/
 
-int x[30], top = -1;
-void PUSH();
-void DISP();
-void POP();
-void PEEK();
-int menu ()
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+void insertionSort(int arr[], int n);
+
+void main()
 {
-  int choice ;
-  printf("\n\n 1.PUSH");
-  printf("\n\n 2.DISP");
-  printf("\n\n 3.POP");
-  printf("\n\n 4.PEEK");
-  printf("\n\n 5.EXIT");
-      printf("\n\n Enter your choice : ");
-      scanf("%d" , &choice);
-  return choice;
-}
- void main ()
- {
-     int ch;
-     do
-     {
-         ch = menu();
-        switch (ch)
+    int arr[100], i, n, x, choice, flag = 0;
+    printf("\t --- WELCOME TO IMPLEMENTATION OF BINARY SEARCH --- \n");
+    printf("\n Enter the number of elements of the array [maximum size = 100] : ");
+    scanf("%d", &n);
+    printf("\n Enter %d elements of the array : \n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf(" %d", &arr[i]);
+    }
+    insertionSort(arr, n);
+    do
+    {
+        printf("\n\n !! -- Operations available -- !!");
+        printf("\n 1. Display Sorted List \t 2. Search a particular value \t 3. Exit");
+        printf("\n Please Enter your choice : ");
+        scanf("%d", &choice);
+        switch (choice)
         {
         case 1:
-               PUSH ();
-               DISP();
-               break;
-        case 2:
-              DISP();
-              break;
-        case 3:
-              POP();
-              DISP();
-              break;
-        case 4:
-             PEEK();
-             DISP();
-             break;
-        case 5:
-             printf("\n\n EXIT ");
-             getch();
-             exit(0);
-        default :
-              printf("\n\n INVALID INPUT ");
-        }
-         getch();
-     }while(ch!=5);
- }
-   void PUSH()
-   {
-     if (top == 29)
-     {
-         printf("\n\n OVERFLOW!");
-           return ;
-     }
-       top ++;
-       printf("\n\n Enter any num : ");
-         scanf("%d" , &x[top]);
-   }
-    void DISP ()
-    {
-        int i;
-        if (top == -1)
         {
-            printf("\n\n STACK IS EMPTY!");
-        }
-        else
-        {
-            for (i=top;i>=0;i--)
+            printf("\n\n The sorted array is : \n");
+            for (i = 0; i < n; i++)
             {
-                printf("\n\n %d" , x[i]);
+                printf(" %d \t", arr[i]);
             }
+            break;
         }
+        case 2:
+        {
+            printf("\n Enter the number to be searched : ");
+            scanf("%d", &x);
+            int beg = 0, end = n - 1, mid;
+            while (beg <= end)
+            {
+                mid = (beg + end) / 2;
+                if (arr[mid] == x)
+                {
+                    printf("\n %d is present in the sorted array at index : %d", x, mid);
+                    flag = 1;
+                    break;
+                }
+                else if (arr[mid] > x)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    beg = mid + 1;
+                }
+            }
+            if (beg > end || flag == 0)
+            {
+                printf("\n %d does not exist int the array", x);
+            }
+            break;
+        }
+        case 3:
+        {
+            printf("\n Program Finished !! Thank You");
+            break;
+        }
+        default:
+        {
+            printf("\n Please enter a valid choice 1, 2, 3.");
+        }
+        }
+    } while (choice != 3);
+    getch();
+}
+
+void insertionSort(int arr[], int n)
+{
+    int i, j, temp;
+    for (i = 1; i < n; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while ((temp < arr[j]) && (j >= 0))
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
     }
-     void POP()
-     {
-         if (top == -1)
-         {
-             printf("\n\n STACK IS EMPTY!");
-         }
-         else
-         {
-             x[top] = 0;
-             top--;
-         }
-     }
-     void PEEK()
-     {
-         if (top == -1)
-         {
-             printf("\n\n STACK IS EMPTY!");
-         }
-         else
-         {
-             printf("\n\n PEEK => %d " , x[top]);
-         }
-     }
+}
